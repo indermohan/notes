cd262064dad403a08dc00df8b4e8deccf1082ccc
unoiaAndroid <unoiadeveloper@gmail.com>
2018-03-05 12:58:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/unoiaandroid/notes/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.unoiaandroid.notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> notes;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listview);
+
+    }
+}
